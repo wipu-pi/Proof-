98ab0b00d47ef708e0b111c42b5dbfb1172eb513
wipu-pi <wipu.pi@gmail.com>
2026-03-20 16:41:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ const server = http.createServer((req, res) => {
       return;
     }
     proxyPost(req, res, 'generativelanguage.googleapis.com',
-      '/v1beta/models/gemini-2.5-flash-preview-04-17:generateContent?key=' + GM_KEY, {});
+      '/v1beta/models/gemini-2.5-flash-lite:generateContent?key=' + GM_KEY, {});
     return;
   }
 
